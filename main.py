a1094d7e2c84c51cd5a737aba77980f9e7223341
EdgarMartinezEs <71104237+EdgarMartinezEsqueda@users.noreply.github.com>
2022-03-22 01:35:54 +0000

    inicial, sin parte gráfica

--- main.py
+++ main.py
@@ -1,124 +1,110 @@
-#Pa' lo grafico 
-import tkinter as tk  #https://realpython.com/python-gui-tkinter/
-#window = tk.Tk()
-
-#https://github.com/mussaiin/Santa-Claus-Problem
-import random
-from threading import Semaphore
-from threading import Thread #pa los hilos
+from threading import Thread, Semaphore
 import time
+import random
+import sys 
 
-#Variables: Elfos, renos, semaforoSanta, semaforoElfo
-#semaforoRenos, renos
-elves_c = 0
-reindeer_c = 0
-santaSem = Semaphore()
-reindeerSem = Semaphore()
-elfTex = Semaphore()
-mutex = Semaphore(1)
+# SEMAFOROS, los números son el valor inicial del contador 
+semSanta = Semaphore(0) # Semáforo de Santa
+semRenos = Semaphore(9) # Semáforos de los renos
+semAyudaRenos = Semaphore(3) # Semáforo para cuando pidan ayuda los renos
+semDuendes = Semaphore(0) # Semáforos duendes
 
-#Funcion prepararTrineo
-def prepareSleigh():
-    global reindeer_c
-    print("Santa Claus: preparando trineo")
+#VARIABLES
+nRenos = 0 # Variable para contabilizar el numero de renos que han llegado
+auxRenos = 0 #Auxuliar para que imprima de mejor manera las cosas
+nDuendes = 0 #   Variable para contabilizar el numero de duendes que han llegado
+duendesEsperando = 0 #Variable que contabiliza el trio de duendes 
 
+            
+nombreRenos = ["RUDOLPH", "BLITZEN", "DONDER", "CUPID", "COMET", "VIXEN", "PRANCER", "DANCER", "DASHER"]
+nombreDuendes = ["Taleasin", "Halafarin", "Ailduin", "Adamar", "Galather", "Estelar", "Lyari", "Andrathath", "Wyn"]
 
-#Funcion para ayudar a los renos
-def helpElves():
-    print("Santa Claus: ayudando elfos")
-
-
-#Funcion para amarrar un reno
-def getHitched():
-    print("Este es un reno ", reindeer_c)
-
-#Funcion que les dice a los elfos que ayuden
-def getHelp():
-    print("Este es un elfo", elves_c)
-
-#Definimos a santa
+# Definicion del proceso Santa: Santa duerme y le despertaran cuand
+# Si tres duendes piden ayuda, Santa los ayudará, liberará otros tres
+# y despues liberara (semDuendes.release()) los tres duendes ayudados. Si
+# el trineo y los liberara (semRenos.release()). Santa se desper
 def santa():
-    global elves_c, reindeer_c
-    print("Santa Claus: Yaye gue, ?qal esel ped2¿?")
+    global nRenos
+    global duendesEsperando
+    print("---- SANTA SE HA PUESTO A DORMIR")
     while True:
-        santaSem.acquire()
-        mutex.acquire()
-      #Si los renos son 9, preparamos el trineo
-        if reindeer_c >= 9:
-            prepareSleigh()
-          #Liberamos al los renos
+        semSanta.acquire() # Decrementa el samaforo, pero como el contador interno no puede ser menor a 0, esperará hasta que se libere
+        print("---- SANTA HA DESPERTADO")
+        if duendesEsperando == 3:
+            duendesEsperando = 0
+            for i in range(3):
+                print("---- SANTA AYUDA AL DUENDE {} DE 3".format(i + 1))
+                semAyudaRenos.release() # desbloqueas para la llegada de 3 duendes mas
+            print("------ SANTA TERMINA DE AYUDAR A LOS DUENDES")
+            for i in range(3):
+                semDuendes.release()
+        elif nRenos == 9:
+            nRenos = 0
+            navidad()
             for i in range(9):
-                reindeerSem.release()
-            print("Santa Claus: Yevenle periko a loz mocoxoz delmun do")
-            reindeer_c -= 9
-            time.sleep(4)
-        elif elves_c == 3:
-            helpElves()
-        mutex.release()
+                semRenos.release()
 
-#Definimos a los renos
-def reindeer():
-    global reindeer_c
+# Definicion del proceso reno: los renos van llegando y se van bloqueando (semRenos.acquire(0).
+# Cuando llegan los 9 renos despiertan a Santa (Santasem.release()).
+# Despues Santa los prepara y los ata al trineo solo una vez, y los libera.
+def reno():
+    global nRenos
+    global auxRenos
+    num = nRenos
+    nRenos += 1
+    print ("Reno {} ha llegado".format (nombreRenos[num]))
+    time.sleep(random. randint (5, 7))     
+    auxRenos += 1     
+    if auxRenos == 9:
+        print("Han llegado los 9 renos")
+        semSanta.release() # Libera el semaforo, aumenta el contador -> santa se despierta
+    else:
+        print("Reno {} listo y enganchado en el trineo, es el {}". format(nombreRenos[num], num))
+    semRenos.acquire()
+
+#Definicion del proceso elfo: como se puede apreciar los duendes van llegando y pediran ayuda dos veces cada uno. (TO_HELP=2)
+# Una vez hayan llegado tres duendes, los siguientes que lleguen seran bloqueados (ELfSemH.acquire()) hasta que Santa los libere.
+# Solo cuando tres duendes pidan ayuda Santa se despertara (Santasem.release()).
+# Los tres duendes que piden ayuda estarán bloqueados (ELfSem.acquire(0) hasta que Santa los ayude y los libere.
+# Cuando cada elfo haya sido ayudado dos veces acabaran.
+def duende():
+    global nDuendes
+    global duendesEsperando
+    num = nDuendes
+    nDuendes += 1
+    print("Duende {} llegando al taller".format (nombreDuendes[num]))
     while True:
-      #Cambiamos el estado del semaforo para que lleguen los 
-      #renos
-        mutex.acquire()
-        reindeer_c += 1
-        if reindeer_c == 9:
-            santaSem.release()
-        mutex.release
-      #Amarramos al reno
-        getHitched()
-        print("El reno numero ", reindeer_c, " ha sido amarrado")
-        reindeerSem.acquire()
-        time.sleep(random.randint(2, 3))
+        time.sleep(random.randint (1,5))
+        semAyudaRenos.acquire() # decrementa el contador
+        duendesEsperando += 1
+        duende = duendesEsperando 
+        if duende < 3:
+            print("Duende {} esperando, es el {} en la fila".format(nombreDuendes[num], duende))
+        elif duende == 3:
+            print("Duende {} esperando: Es el {}, entre los tres llaman a Santa!". format(nombreDuendes[num], duende))
+            semSanta.release() # Libera el semaforo, aumenta el contador -> santa se despierta
+        semDuendes.acquire() # Decrementa el contador de duendes
 
-#Definimos a los elfos
-def elves():
-    global elves_c
-    while True:
-      #Preparamos los semaforos para el funcionamiento
-        elfTex.acquire()
-        mutex.acquire()
-      #Agregamos elfos para que se pueda continuar con el problema
-        elves_c += 1
-        if elves_c == 3:
-          #Despertamos a santa
-            santaSem.release()
-        else:
-          #Aumentamos el semaforo
-            elfTex.release()
-        mutex.release()
-        getHelp()
-        time.sleep(random.randint(2, 5))
-        mutex.acquire()
-        elves_c -= 1
-        if elves_c == 0:
-            elfTex.release()
-        mutex.release()
-        print("Duende ", elves_c, "at work")
+# Santa prepara el trineo y se va a dormir
+def navidad():
+    print(" ---- SANTA SE VA EN SU TRINEO")
+    print(" ... DESPUES DE ESTO, SANTA VUELVE A DORMIR")
+    sys.exit()
 
 
-elfThread = []  # threads for elves
-reindThread = []  # threads from reindeers
-
-
-def main():
-    thread = Thread(target=santa)  # main thread for SantaClaus
-    thread.start()  # starting the thread
-    for i in range(9):
-        reindThread.append(Thread(target=reindeer))
-    for j in range(9):
-        elfThread.append(Thread(target=elves))
-    for t in elfThread:
-        t.start()
-    for t in reindThread:
-        t.start()
-    for t in elfThread:
-        t.join()
-    for t in reindThread:
-        t.join()
-    thread.join()
-
-
-main()
+# Array de hilos
+threads = []
+# Santa
+threads.append(Thread(target=santa))
+# Duendes
+for i in range(9):
+    threads.append(Thread(target=duende))
+# Reno
+for i in range(9):
+    threads.append(Thread(target=reno))
+# Iniciamos todos los hilos
+for t in threads:
+    t.start()
+# Esperamos a que se completen todos los hilos
+for t in threads:
+    t.join()
