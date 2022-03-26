4c4dfc2f863914029639c37366369c76521e6fa6
EdgarMartinezEsqueda <71104237+EdgarMartinezEsqueda@users.noreply.github.com>
2022-03-26 13:22:08 -0600

    Update santa.py

--- santa.py
+++ santa.py
@@ -2,7 +2,6 @@
 from threading import Thread, Semaphore
 import time
 import random
-import sys 
 
 # SEMAFOROS, los números son el valor inicial del contador 
 semSanta = Semaphore(0) # Semáforo de Santa
@@ -12,75 +11,65 @@ semDuendes = Semaphore(0) # Semáforos duendes
 
 #VARIABLES
 nRenos = 0 # Variable para contabilizar el numero de renos que han llegado
-auxRenos = 0 #Auxuliar para que imprima de mejor manera las cosas
-nDuendes = 0 #   Variable para contabilizar el numero de duendes que han llegado
 duendesEsperando = 0 #Variable que contabiliza el trio de duendes 
+regalosEntregados = False
 
 # Nombres, pues porque sí
 nombreRenos = ["RUDOLPH", "BLITZEN", "DONDER", "CUPID", "COMET", "VIXEN", "PRANCER", "DANCER", "DASHER"]
-nombreDuendes = ["Tomás Turbado", "Meardel Peson", "Lalo Onganiza", "Carmelo Tallas", "Elsa Porrico", "Aquiles Pico", "Marisa Caleche", "Carmela Rosas", "Pato Carlo"]
 
 # Hilo de Santa
 def santa():
     global nRenos
     global duendesEsperando
-    print("---- SANTA SE HA PUESTO A DORMIR")
-    for _ in range (7):
+    print("---- SANTA ESTÁ DORMIDO")
+    while nRenos < 9:
         semSanta.acquire() # Decrementa el samaforo, pero como el contador interno no puede ser menor a 0, esperará hasta que se libere
         print("---- SANTA HA DESPERTADO")
         if duendesEsperando == 3:   #Si lo están esperando 3 duendes...
             duendesEsperando = 0    #Se resetea el contador
             for i in range(3):  #Liberamos
                 print("---- SANTA AYUDA AL DUENDE {} DE 3".format(i + 1))
+                time.sleep(random.randint (1,3))   
                 semAyudaDuendes.release() # desbloqueas para la llegada de 3 duendes mas
             print("------ SANTA TERMINA DE AYUDAR A LOS DUENDES")
             for i in range(3):  #Ahora se liberan 3 espacios para duendes
                 semDuendes.release()
-        elif nRenos == 9:
-            nRenos = 0
-            navidad()
+    navidad()
 
 # Hilo de los renos
 def reno():
     global nRenos
-    global auxRenos
-    num = nRenos
-    nRenos += 1
-    print ("Reno {} ha llegado".format (nombreRenos[num]))
-    time.sleep(random. randint (5, 7))     
-    auxRenos += 1     
-    if auxRenos == 9:   #Cuando sean 9 renos, se libera el semaforo de Santa
-        print("Han llegado los 9 renos")
-        semSanta.release() # Libera el semaforo, aumenta el contador -> santa se despierta
-    else:
-        print("Reno {} listo y enganchado en el trineo, es el {}". format(nombreRenos[num], num))
-    semRenos.acquire()
+    while nRenos < 9:
+        time.sleep(random.randint(5,7))    
+        nRenos += 1
+        print("Reno {} listo y enganchado en el trineo, es el {}". format(nombreRenos[nRenos-1], nRenos))
+        semRenos.acquire()
+    print("--- LOS 9 RENOS ESTÁN LISTOS ESPERANDO A SANTA")
+    semSanta.release() # Libera el semaforo, aumenta el contador -> santa se despierta
+
 
 # Hilo de los duendes
 def duende():
-    global nDuendes
     global duendesEsperando
-    num = nDuendes
-    nDuendes += 1
-    print("Duende {} llegando al taller".format (nombreDuendes[num]))
-    for _ in range(2):
+    global regalosEntregados
+    while not regalosEntregados:
         time.sleep(random.randint (1,5))
         semAyudaDuendes.acquire() # decrementa el contador de que los duendes ocupan a Santa... ¿Por qué? Porque si no lo tuviera, llegarían más y más duendes sin parar
         duendesEsperando += 1
-        duende = duendesEsperando 
-        if duende < 3:  #Si aún no llegan a ser 3 duendes... se agrega a la espera
-            print("Duende {} esperando, es el {} en la fila".format(nombreDuendes[num], duende))
-        elif duende == 3:   #Cuando sean los 3, liberan el semáforo de Santa
-            print("Duende {} esperando: Es el {}, entre los tres llaman a Santa!". format(nombreDuendes[num], duende))
+        if duendesEsperando < 3:  #Si aún no llegan a ser 3 duendes... se agrega a la espera
+            print("Duende esperando, es el {} en la fila".format(duendesEsperando))
+        elif duendesEsperando == 3:   #Cuando sean los 3, liberan el semáforo de Santa
+            print("Llega el 3er duende, entre los tres llaman a Santa!")
             semSanta.release() # Libera el semaforo, aumenta el contador -> santa se despierta
-        semDuendes.acquire() # Decrementa el contador de duendes
 
 # Santa prepara el trineo y se va a dormir
 def navidad():
-    for i in range(9):  #Libreramos los 9 renos
+    global regalosEntregados
+    for _ in range(9):  #Libreramos los 9 renos
         semRenos.release() 
     print(" ---- SANTA SE VA EN SU TRINEO")
     print(" ... DESPUES DE ESTO, SANTA VUELVE A DORMIR")
+    regalosEntregados = True
 
 def inicio():
     # Array de hilos
@@ -88,15 +77,12 @@ def inicio():
     # Santa
     threads.append(Thread(target=santa))
     # Duendes
-    for i in range(9):
-        threads.append(Thread(target=duende))
+    threads.append(Thread(target=duende))
     # Reno
-    for i in range(9):
-        threads.append(Thread(target=reno))
+    threads.append(Thread(target=reno))
     # Iniciamos todos los hilos
     for t in threads:
         t.start()
     # Esperamos a que se completen todos los hilos
     for t in threads:
         t.join()
-    print("A")
