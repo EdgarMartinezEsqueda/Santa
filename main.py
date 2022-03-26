8316e0982ff082080d2289be107a9a66e7dc97f2
EdgarMartinezEsqueda <71104237+EdgarMartinezEsqueda@users.noreply.github.com>
2022-03-25 18:57:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+#Librerías básicas
 from threading import Thread, Semaphore
 import time
 import random
 import sys 
+#Librerías para lo gráfico
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog, QGridLayout
+from PyQt5.QtGui import QPixmap
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtGui import QCursor
+
+
 
 # SEMAFOROS, los números son el valor inicial del contador 
 semSanta = Semaphore(0) # Semáforo de Santa
 semRenos = Semaphore(9) # Semáforos de los renos
-semAyudaRenos = Semaphore(3) # Semáforo para cuando pidan ayuda los renos
+semAyudaDuendes = Semaphore(3) # Semáforo para cuando pidan ayuda los renos
 semDuendes = Semaphore(0) # Semáforos duendes
 
 #VARIABLES
@@ -15,14 +23,11 @@ auxRenos = 0 #Auxuliar para que imprima de mejor manera las cosas
 nDuendes = 0 #   Variable para contabilizar el numero de duendes que han llegado
 duendesEsperando = 0 #Variable que contabiliza el trio de duendes 
 
-            
+# Nombres, pues porque sí
 nombreRenos = ["RUDOLPH", "BLITZEN", "DONDER", "CUPID", "COMET", "VIXEN", "PRANCER", "DANCER", "DASHER"]
-nombreDuendes = ["Taleasin", "Halafarin", "Ailduin", "Adamar", "Galather", "Estelar", "Lyari", "Andrathath", "Wyn"]
+nombreDuendes = ["Tomás Turbado", "Meardel Peson", "Lalo Onganiza", "Carmelo Tallas", "Elsa Porrico", "Aquiles Pico", "Marisa Caleche", "Carmela Rosas", "Pato Carlo"]
 
-# Definicion del proceso Santa: Santa duerme y le despertaran cuand
-# Si tres duendes piden ayuda, Santa los ayudará, liberará otros tres
-# y despues liberara (semDuendes.release()) los tres duendes ayudados. Si
-# el trineo y los liberara (semRenos.release()). Santa se desper
+# Hilo de Santa
 def santa():
     global nRenos
     global duendesEsperando
@@ -30,23 +35,19 @@ def santa():
     while True:
         semSanta.acquire() # Decrementa el samaforo, pero como el contador interno no puede ser menor a 0, esperará hasta que se libere
         print("---- SANTA HA DESPERTADO")
-        if duendesEsperando == 3:
-            duendesEsperando = 0
-            for i in range(3):
+        if duendesEsperando == 3:   #Si lo están esperando 3 duendes...
+            duendesEsperando = 0    #Se resetea el contador
+            for i in range(3):  #Liberamos
                 print("---- SANTA AYUDA AL DUENDE {} DE 3".format(i + 1))
-                semAyudaRenos.release() # desbloqueas para la llegada de 3 duendes mas
+                semAyudaDuendes.release() # desbloqueas para la llegada de 3 duendes mas
             print("------ SANTA TERMINA DE AYUDAR A LOS DUENDES")
-            for i in range(3):
+            for i in range(3):  #Ahora se liberan 3 espacios para duendes
                 semDuendes.release()
         elif nRenos == 9:
             nRenos = 0
             navidad()
-            for i in range(9):
-                semRenos.release()
 
-# Definicion del proceso reno: los renos van llegando y se van bloqueando (semRenos.acquire(0).
-# Cuando llegan los 9 renos despiertan a Santa (Santasem.release()).
-# Despues Santa los prepara y los ata al trineo solo una vez, y los libera.
+# Hilo de los renos
 def reno():
     global nRenos
     global auxRenos
@@ -55,18 +56,14 @@ def reno():
     print ("Reno {} ha llegado".format (nombreRenos[num]))
     time.sleep(random. randint (5, 7))     
     auxRenos += 1     
-    if auxRenos == 9:
+    if auxRenos == 9:   #Cuando sean 9 renos, se libera el semaforo de Santa
         print("Han llegado los 9 renos")
         semSanta.release() # Libera el semaforo, aumenta el contador -> santa se despierta
     else:
         print("Reno {} listo y enganchado en el trineo, es el {}". format(nombreRenos[num], num))
     semRenos.acquire()
 
-#Definicion del proceso elfo: como se puede apreciar los duendes van llegando y pediran ayuda dos veces cada uno. (TO_HELP=2)
-# Una vez hayan llegado tres duendes, los siguientes que lleguen seran bloqueados (ELfSemH.acquire()) hasta que Santa los libere.
-# Solo cuando tres duendes pidan ayuda Santa se despertara (Santasem.release()).
-# Los tres duendes que piden ayuda estarán bloqueados (ELfSem.acquire(0) hasta que Santa los ayude y los libere.
-# Cuando cada elfo haya sido ayudado dos veces acabaran.
+# Hilo de los duendes
 def duende():
     global nDuendes
     global duendesEsperando
@@ -75,21 +72,23 @@ def duende():
     print("Duende {} llegando al taller".format (nombreDuendes[num]))
     while True:
         time.sleep(random.randint (1,5))
-        semAyudaRenos.acquire() # decrementa el contador
+        semAyudaDuendes.acquire() # decrementa el contador de que los duendes ocupan a Santa... ¿Por qué? Porque si no lo tuviera, llegarían más y más duendes sin parar
         duendesEsperando += 1
         duende = duendesEsperando 
-        if duende < 3:
+        if duende < 3:  #Si aún no llegan a ser 3 duendes... se agrega a la espera
             print("Duende {} esperando, es el {} en la fila".format(nombreDuendes[num], duende))
-        elif duende == 3:
+        elif duende == 3:   #Cuando sean los 3, liberan el semáforo de Santa
             print("Duende {} esperando: Es el {}, entre los tres llaman a Santa!". format(nombreDuendes[num], duende))
             semSanta.release() # Libera el semaforo, aumenta el contador -> santa se despierta
         semDuendes.acquire() # Decrementa el contador de duendes
 
 # Santa prepara el trineo y se va a dormir
 def navidad():
+    for i in range(9):  #Libreramos los 9 renos
+        semRenos.release() 
     print(" ---- SANTA SE VA EN SU TRINEO")
     print(" ... DESPUES DE ESTO, SANTA VUELVE A DORMIR")
-    sys.exit()
+    sys.exit()  #Comentar si se quiere hacer infinitamente
 
 
 # Array de hilos
