d3f1cf833fc47b286f734c1abe42c08fb6276441
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2022-03-28 16:18:10 -0600

    actualiza al momento

--- interfaz.py
+++ interfaz.py
@@ -16,30 +16,17 @@ ventana.show()
 #----------------CUADRICULA (GRID)----------------
 grid = QGridLayout()
 
-#----------------BOTONES----------------
-botonSalir = QPushButton("Salir")
-botonSalir.setStyleSheet("width: 20px;")
-botonIniciar = QPushButton("Iniciar")
-botonIniciar.clicked.connect(santa.inicio)
-botonIniciar.setStyleSheet("width: 20px;")
-
-#Agregamos los elementos al grid
-grid.addWidget(botonSalir,2,2)
-grid.addWidget(botonIniciar,2,3)
-
-#----------------GIFS----------------
-# imgSnta = QMovie("imagenes/repartiendo.gif")
-# imagen = QLabel()
-# imagen.setMovie(imgSnta)
-# grid.addWidget(imagen, 0, 0)
-
-
 #----------------IMÁGENES----------------
 ctnCentral = QLabel()
 ctnCentral.setStyleSheet("background: #ff05ef")
-imgcaliz = QPixmap("imagenes/renos.png")
-imgcaliz = imgcaliz.scaledToHeight(80) #redimensionar la imagen
-ctnCentral.setPixmap(imgcaliz)
+# imgcaliz = QPixmap("imagenes/renos.png")
+# imgcaliz = imgcaliz.scaledToHeight(80) #redimensionar la imagen
+# ctnCentral.setPixmap(imgcaliz)
+
+#----------------GIFS----------------
+# gif = QMovie("imagenes/repartiendo.gif")
+# ctnCentral.setMovie(gif)
+# gif.start()
 
 #renos
 ctnRenos = QLabel()
@@ -73,9 +60,9 @@ tituloSanta = QLabel('Santa\nDormido')
 tituloSanta.setStyleSheet("font-size:20px; font-family:'sans-serif'; text-align: center;")
 tituloSanta.setAlignment(Qt.AlignCenter)
 
-tituloDuentes = QLabel('Duentes\n0')
-tituloDuentes.setStyleSheet("font-size:20px; font-family:'sans-serif'; text-align: center;")
-tituloDuentes.setAlignment(Qt.AlignCenter)
+tituloDuendes = QLabel('Duentes\n0')
+tituloDuendes.setStyleSheet("font-size:20px; font-family:'sans-serif'; text-align: center;")
+tituloDuendes.setAlignment(Qt.AlignCenter)
 
 tituloRenos = QLabel('Renos\n0')
 tituloRenos.setStyleSheet("font-size:20px; font-family:'sans-serif'; text-align: center;")
@@ -83,13 +70,26 @@ tituloRenos.setAlignment(Qt.AlignCenter)
 
 #Agregamos los elementos al grid
 grid.addWidget(tituloSanta,0,5)
-grid.addWidget(tituloDuentes,0,1)
+grid.addWidget(tituloDuendes,0,1)
 grid.addWidget(tituloRenos,0,3)
 
 grid.setRowStretch(0, 1)
 grid.setRowStretch(1, 5)
 grid.setRowStretch(2, 1)
 
+#----------------BOTONES----------------
+botonSalir = QPushButton("Salir")
+botonSalir.setStyleSheet("width: 20px;")
+botonIniciar = QPushButton("Iniciar")
+botonIniciar.clicked.connect(lambda: santa.inicio(tituloSanta, tituloDuendes, tituloRenos, ctnCentral))
+# botonIniciar.clicked.connect(santa.inicio)
+botonIniciar.setStyleSheet("width: 20px;")
+
+#Agregamos los elementos al grid
+grid.addWidget(botonSalir,2,2)
+grid.addWidget(botonIniciar,2,3)
+
 #----------------EJECUCIÓN----------------
 ventana.setLayout(grid)
 sys.exit(app.exec())
+
--- santa.py
+++ santa.py
@@ -2,6 +2,7 @@
 from threading import Thread, Semaphore
 import time
 import random
+from PyQt5.QtGui import QMovie
 
 # SEMAFOROS, los números son el valor inicial del contador 
 semSanta = Semaphore(0) # Semáforo de Santa
@@ -16,13 +17,14 @@ regalosEntregados = False
 nombreRenos = ["RUDOLPH", "BLITZEN", "DONDER", "CUPID", "COMET", "VIXEN", "PRANCER", "DANCER", "DASHER"]
 
 # Hilo de Santa
-def santa():
+def santa(label, cntCentral):
     global nRenos
     global duendesEsperando
     print("---- SANTA ESTÁ DORMIDO")
     while nRenos < 9:
         semSanta.acquire() # Decrementa el samaforo, pero como el contador interno no puede ser menor a 0, esperará hasta que se libere
         print("---- SANTA HA DESPERTADO")
+        label.setText("Santa\nDESPIERTO")
         if duendesEsperando == 3:   #Si lo están esperando 3 duendes...
             duendesEsperando = 0    #Se resetea el contador
             for i in range(3):  #Liberamos
@@ -30,32 +32,36 @@ def santa():
                 time.sleep(random.randint (1,3))   
                 semAyudaDuendes.release() # desbloqueas para la llegada de 3 duendes mas
             print("------ SANTA TERMINA DE AYUDAR A LOS DUENDES")
+            label.setText("Santa\nDORMIDO")
     navidad()
 
 # Hilo de los renos
-def reno():
+def reno(label):
     global nRenos
     while nRenos < 9:
         time.sleep(random.randint(5,7))    
         nRenos += 1
+        label.setText("Renos\n{}".format(nRenos))
         print("Reno {} listo y enganchado en el trineo, es el {}". format(nombreRenos[nRenos-1], nRenos))
     print("--- LOS 9 RENOS ESTÁN LISTOS ESPERANDO A SANTA")
     semSanta.release() # Libera el semaforo, aumenta el contador -> santa se despierta
 
 
 # Hilo de los duendes
-def duende():
+def duende(label):
     global duendesEsperando
     global regalosEntregados
     while not regalosEntregados:
         time.sleep(random.randint (1,5))
         semAyudaDuendes.acquire() # decrementa el contador de que los duendes ocupan a Santa... ¿Por qué? Porque si no lo tuviera, llegarían más y más duendes sin parar
         duendesEsperando += 1
+        label.setText("Duendes\n{}".format(duendesEsperando))
         if duendesEsperando < 3:  #Si aún no llegan a ser 3 duendes... se agrega a la espera
             print("Duende esperando, es el {} en la fila".format(duendesEsperando))
         elif duendesEsperando == 3:   #Cuando sean los 3, liberan el semáforo de Santa
             print("Llega el 3er duende, entre los tres llaman a Santa!")
             semSanta.release() # Libera el semaforo, aumenta el contador -> santa se despierta
+            label.setText("Duendes\n0")
 
 # Santa prepara el trineo y se va a dormir
 def navidad():
@@ -64,18 +70,15 @@ def navidad():
     print(" ... DESPUES DE ESTO, SANTA VUELVE A DORMIR")
     regalosEntregados = True
 
-def inicio():
+def inicio(lbSanta, lbDuendes, lbRenos, cntCentral):
     # Array de hilos
     threads = []
     # Santa
-    threads.append(Thread(target=santa))
+    threads.append(Thread(target=santa, args = (lbSanta, cntCentral) ))
     # Duendes
-    threads.append(Thread(target=duende))
+    threads.append(Thread(target=duende, args = (lbDuendes,) ))
     # Reno
-    threads.append(Thread(target=reno))
+    threads.append(Thread(target=reno, args = (lbRenos,) ))
     # Iniciamos todos los hilos
     for t in threads:
         t.start()
-    # Esperamos a que se completen todos los hilos
-    for t in threads:
-        t.join()
